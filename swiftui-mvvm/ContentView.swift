2ae39d6818c3e9e5315d96ce440b732682d7fe37
AnthonyBuilder <anthonyJ2017@icloud.com>
2021-08-25 19:26:16 -0300

    Introduzindo a arquitetura MVVM para Testes Unitarios

--- swiftui-mvvm/ContentView.swift
+++ swiftui-mvvm/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject private var model: ContentViewModel
+    
+    init(model: ContentViewModel) {
+        self.model = model
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text(model.state.isLoading ? "Carregando..." : model.state.message)
+            .onAppear(perform: model.loadData)
+    }
+}
+
+struct ContentViewState {
+    var isLoading = false
+    var message = ""
+}
+
+class ContentViewModel: ObservableObject {
+    @Published private(set) var state: ContentViewState
+    
+    init(initialState: ContentViewState = .init()) {
+        state = initialState
+    }
+    
+    func loadData() {
+        state.isLoading = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3)) {
+            self.state.isLoading = false
+            self.state.message = "Olá mundo !"
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView(model: .init(initialState: .init(isLoading: true)))
+                .previewDisplayName("Loading")
+            ContentView(model: .init(initialState: .init(isLoading: false, message: "Olá Mundo")))
+                .previewDisplayName("Loaded")
+        }
     }
 }
