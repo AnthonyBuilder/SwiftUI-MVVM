4665b71fe9fa7afd73f8e89ef29ea6514f5f7d07
AnthonyBuilder <anthonyJ2017@icloud.com>
2021-08-25 15:02:49 -0300

    Initial Commit

---
+++ swiftui-mvvm/swiftui_mvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_mvvmApp.swift
+//  swiftui-mvvm
+//
+//  Created by Anthony on 25/08/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_mvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
